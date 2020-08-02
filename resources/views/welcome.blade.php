f81f93f60f08f4359ad17eda982ae56af10273d9
Joshua Etim <jetimworks@gmail.com>
2020-08-02 21:38:07 +0100

    MM

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,98 +1,191 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<html lang="en">
+
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Invest Script</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+      <title>Invest Script</title>
+    
+      <!-- Fonts -->
+      <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    
+      <!-- Styles -->
+      <style>
+        html,
+        body {
+          background-color: #fff;
+          color: #636b6f;
+          font-family: 'Nunito', sans-serif;
+          font-weight: 200;
+          height: 100%;
+          margin: 0;
+          line-height: 1.4;
+          font-size: 62.5%;
+        }
+    
+        .full-height {
+          height: 100vh;
+        }
+    
+        .flex-center {
+          align-items: center;
+          display: flex;
+          justify-content: center;
+          overflow: hidden;
+        }
+    
+        .works {
+          align-items: center;
+          display: flex;
+          justify-content: center;
+          flex-direction: column;
+          overflow: hidden;
+          font-size: 3rem;
+          width: 85%;
+          margin-left: auto;
+          margin-right: auto;
+          padding-bottom: 5rem;
+        }
+    
+        .works p {
+          font-size: 1.6rem;
+          font-weight: 700;
+          margin-bottom: -.5rem;
+        }
+    
+        .works-content {
+          width: 85%;
+          margin-left: auto;
+          margin-right: auto;
+        }
+    
+        .works h3 {
+          font-size: 2.2rem;
+          font-weight: 700;
+          margin-bottom: -.8rem;
+          text-decoration: underline;
+          font-weight: 900;
+        }
+    
+        .works h4 {
+          font-size: 1.8rem;
+          font-weight: 700;
+          text-decoration: underline;
+          font-weight: 900;
+          margin-bottom: -.8rem;
+        }
+    
+        .position-ref {
+          position: relative;
+        }
+    
+        .top-right {
+          position: absolute;
+          right: 1rem;
+          top: 1.8rem;
+        }
+    
+        .content {
+          text-align: center;
+        }
+    
+        .title p {
+          font-size: 8rem;
+        }
+    
+        @media screen and (max-width: 35.75em) {
+          .title p {
+            font-size: 4rem;
+          }
+    
+        }
+    
+        .links>a {
+          color: #636b6f;
+          padding: 0 2.5rem;
+          font-size: 1.3rem;
+          font-weight: 600;
+          letter-spacing: .1rem;
+          text-decoration: none;
+          text-transform: uppercase;
+        }
+    
+        .m-b-md {
+          margin-bottom: 3rem;
+        }
+    
+        tabs:hover {
+          display: inline-block;
+          color: firebrick;
+          transform: scale(1.2);
+        }
+      </style>
     </head>
+    
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    {{config('app.name')}} - Coming Soon
-                </div>
-
-                <div class="links">
-                    @auth
-                        <a href="{{url('/home')}}">Home</a>
-                    @else
-                        <a href="{{route('login')}}">Login</a>
-                        <a href="{{route('register')}}?ref={{session('referral') ?? '1'}}">Register</a>
-                    @endauth
-                </div>
-            </div>
+      <div class="works">
+        <h2><u><b>Invest Script - How It Works</b></u></h2>
+        <h3>Admin Dashboard:</h3>
+        <div class="works-content">
+          <p>1. To merge withdrawals manually go to the Manual Merging Panel to merge users to a particular user and click
+            the proceed to merge button. </p>
+          <p>2. Admin can reply to users supports and complaints from his dasboard.</p>
+          <p>3. Admin can Delete, Block and Unblock Users as he wishes.</p>
+          <p>4. Admin can see Merged Donations, in-progress Donations and Expired Donations.</p>
+          <p>5. Admin can Create Special Users. From his Dashboards. The specials users are the ones collecting the
+            activation
+            fees. <strong>The special user will be the one taking all the initial investments. The admin will login as the
+              special users to approve payment. </strong></p>
+          <p>6. Auto rematch (when a transaction is timed out. the unpaid member transaction goes back to GH list for a
+            rematch manual/auto rematch while the defaulter automatically blocked).</p>
+          <p>7. In-Active users are blocked automatically. can also be unblocked by the admin.</p>
+          <p>8. Admin has to create a user before lauching the site and also input the amount of money he wants the user to
+            withdrawal so that other users who register can be merged to it at first.</p>
         </div>
+    
+        <h3>User Dashboard:</h3>
+        <div class="works-content">
+          <p>1. Recommitment: Recommit same amount or higher and get confirmed before a user will be eligible to Withdraw
+            total meant to receive.</p>
+          <p>2. Activation fee has now been added. User cannot become full member of the system unless He or she pays an
+            activation fee. User can't perform any task until he or she pays an activation fee. </p>
+          <p>3. Referral: referral program.</p>
+          <p>4. NO package – This means user can enter an amount they which to donate to the system according to the
+            minimum and maximum amount set by the admin.</p>
+          <p>5.Timer is available for User whenever he or she makes an Investment with a 3-days interval before he or she
+            can withdraw an investment.</p>
+          <p>6. Note: The merging process happens immediately for testing purposes. For actual usage, it should be set
+            for end ofthe day.</p>
+          <p>7. Users can make complaints over investment and also upload proof and user can also contact admin through
+            the support panel.</p>
+          <p>8. There are so many other features.</p>
+        </div>
+        <h3>SCRIPT LOGIN DETAILS:</h3>
+        <div class="works-content">
+          <p>Homepage: <a href="https://investdemo.herokuapp.com">https://investdemo.herokuapp.com </a></p>
+          <h4>Admin details</h4>
+          <p>
+            Admin Dashboard: <a href="https://investdemo.herokuapp.com/admin">https://investdemo.herokuapp.com/admin </a>
+          </p>
+          <p>Email: admin@gmail.com</p>
+          <p>Password: admin</p>
+    
+          <h4>Users details</h4>
+          <p>
+            User Dashboard: <a href="https://investdemo.herokuapp.com/home">https://investdemo.herokuapp.com/home </a>
+          </p>
+          <p>Email: jimmy@gmail.com</p>
+          <p>Password: password</p>
+    
+          <h4> Special User details</h4>
+          <p>
+            User Dashboard: <a href="https://investdemo.herokuapp.com/home">https://investdemo.herokuapp.com/home </a>
+          </p>
+          <p>Email: paulolu@gmail.com</p>
+          <p>Password: password</p>
+        </div>
+      </div>
     </body>
-</html>
+    
+    </html>
